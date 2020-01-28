f4e32e18972efec11a71073c86785f6b84fa3c2c
Trevor Smith <smittrevor@gmail.com>
2020-01-28 12:15:48 -0800

    Add Axios as dependency, implement working api request in /search POST route

--- javascript/routes/search.js
+++ javascript/routes/search.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 var us_states = require('../us_state.js');
 const generateAddressObj = require('../utils/generateAddressObj');
 const generateOCDID = require('../utils/generateOCDID');
@@ -7,11 +8,15 @@ const generateApiReqUrl = require('../utils/generateApiReqUrl');
 
 /* POST search endpoint. */
 router.post('/', function(req, res, next) {
+  const reqHeaders = {headers: {Accept: 'application/json'}};
   const address = generateAddressObj(req.body);
   const OCDID = generateOCDID(address);
   const apiReqUrl = generateApiReqUrl(OCDID.state, OCDID.place);
 
-  console.log(apiReqUrl);
+
+  axios.get(apiReqUrl, reqHeaders).then(response => {
+    console.log(response.data);
+  })
 
   res.render('index', { title: 'Find My Election', states: us_states });
 });
