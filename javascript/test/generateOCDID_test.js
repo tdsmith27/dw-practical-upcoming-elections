1dc038ec3a61fa0d139565b1c80a600e4431e113
Trevor Smith <smittrevor@gmail.com>
2020-01-28 13:19:16 -0800

    Implement tests for generateOCDID utility function

---
+++ javascript/test/generateOCDID_test.js
@@ -0,0 +1,15 @@
+const tap = require('tap');
+const generateOCDID = require('../utils/generateOCDID');
+
+tap.test('generateOCDID correctly formats state and place OCDID values', t => {
+  const addressObj = {state: "tx", city: "college_station"};
+  const stateOCDID = `ocd-division/country:us/state:tx`;
+  const placeOCDID = `ocd-division/country:us/state:tx/place:college_station`;
+  const OCDIDObj = {state: stateOCDID, place: placeOCDID};
+
+  t.equal(stateOCDID, generateOCDID(addressObj).state, "Correctly generates state OCDID from formatted address object");
+  t.equal(placeOCDID, generateOCDID(addressObj).place, "Coarrectly generates place OCDID from formatted address object");
+  t.same(OCDIDObj, generateOCDID(addressObj), "Correctly generates OCDID object from formatted address object");
+
+  t.end();
+})
